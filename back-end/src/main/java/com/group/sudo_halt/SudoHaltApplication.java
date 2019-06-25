cbc9aee7e623f9bd5fac9a5fad62439039049b58
Tingyeu Yang <ytingyeu@asu.edu>
2019-06-25 12:02:05 -0700

    init for front/back end folder

---
+++ back-end/src/main/java/com/group/sudo_halt/SudoHaltApplication.java
@@ -0,0 +1,13 @@
+package com.group.sudo_halt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudoHaltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudoHaltApplication.class, args);
+	}
+
+}
